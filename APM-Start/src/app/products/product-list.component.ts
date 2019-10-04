1e448155c18beaafb43d8020b77809b243b0fedc
Bo Amusa <Boamusa@gmail.com>
2019-10-03 22:26:56 -0400

    Adding HTTP client module remove hard code data

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string;
 
   private _listFilter: string;
 
@@ -48,7 +49,12 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => (this.errorMessage = err)
+    });
   }
 }
